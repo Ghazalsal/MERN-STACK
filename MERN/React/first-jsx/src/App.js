a6b562a55557f50f72cbbbf93304d3f9e1d20372
Ghazalsal <ghazalsalameh@hotmail.com>
2021-07-13 11:59:24 +0300

    first JSX assignment

---
+++ MERN/React/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I Need To Do:</h2>
+      <ul>
+      <li>Learn React</li>
+      <li> Climb Mt Everst</li>
+      <li>Run a marathon</li>
+      <li>Feed The Dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
